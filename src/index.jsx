3eb0e9dfe10cdecfd00b5f9fd769065d12a4f68d
tofusupreme <yunishbacon@gmail.com>
2022-03-24 11:48:35 +0900

    added components

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,9 @@
 import '../assets/stylesheets/application.scss';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Greeting extends React.Component {
-  render() {
-    const { name, age } = this.props;
-    const message = `Hello I'm ${name} and I'm ${age} years old`;
-    return <h1>{message}</h1>;
-  }
-}
+import App from './components/App';
+
 const root = document.getElementById('root');
 
-ReactDOM.render(<Greeting name="Regulus" age="0" />, root);
+ReactDOM.render(<App />, root);
